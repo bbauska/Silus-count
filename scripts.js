66f95104538048b62fa7797c105320808436818c
Brian Bauska <brianbauska@gmail.com>
2025-01-13 21:44:41 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+<!-- ./js/scripts.js in Silus-counter.bauska.org -->
+// Load the Numeral.js library from a CDN
+<script src="https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js">
+</script>
+let counter = 10100;  /* from Dec 7th/8th */
+
+function count() {
+  counter++;
+  document.getElementById('number').innerHTML = counter;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('clicker').onclick = count;
+    // Format the number using the Numeral.js library
+var formattedNum = numeral(clicker).format("0,0.00");
+	})
